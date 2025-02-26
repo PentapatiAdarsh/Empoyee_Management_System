764936b3dcd8c0f4fe46f1e0b9789452bfccb209
PentapatiAdarsh <APentapati@clarku.edu>
2025-02-26 01:35:54 -0500

    home page UI

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, request
+
+from .models import *
+import random as r
+
+
+
+# Creating a views here.
+def homepage(request):
+    return render(request, 'index.html')
+
